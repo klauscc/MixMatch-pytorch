1dc76af721bf49609feb63bf734a8b3f3e833711
chengfeng <klaus.cheng@qq.com>
2020-11-19 17:11:00 -0500

    resume

--- scripts/train/cifar10-1000-mm0_1_2.sh
+++ scripts/train/cifar10-1000-mm0_1_2.sh
@@ -1,5 +1,7 @@
 name=`basename "$0"`
+workspace=/home/fengchan/stor/workspace/courses/comp790-dl3d/mixmatch/$name
 OMP_NUM_THREADS=8 python train.py \
     --gpu 5 --n-labeled 1000 \
-    --out /home/fengchan/stor/workspace/courses/comp790-dl3d/mixmatch/$name \
+    --out $workspace \
     --layers_mix -2 \
+    --resume $workspace/checkpoint.pth.tar \
